b728a8a5b6246c45429c7a622b04ed1fb9f152e9
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 14:30:27 +0700

    Add UnderscoreLiteral.kt

---
+++ src/main/kotlin/UnderscoreLiteral.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val oneMillion = 1_000_000
+    val creditCardNumber = 1234_5678_9012_3456L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes =  0b11010010_01101001_10010100_10010010
+    println("oneMillion = "+oneMillion)
+    println("creditCardnumber = "+creditCardNumber)
+    println("socialSecurityNumber = "+socialSecurityNumber)
+    println("hexBytes = "+hexBytes)
+    println("bytes = "+bytes)
+}
