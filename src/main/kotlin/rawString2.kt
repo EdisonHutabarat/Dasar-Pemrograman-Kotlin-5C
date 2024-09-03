b055c6da335ab512a536f31fef4056ff655b59db
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 22:22:01 +0700

    add rawString2.kt

---
+++ src/main/kotlin/rawString2.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var text = """
+            |Tell me and I forget.
+            |Teach me and I remember.
+            |Involve me and I learn.
+            |(Benjamin Franklin)
+            """.trimMargin()
+    println(text)
+}
