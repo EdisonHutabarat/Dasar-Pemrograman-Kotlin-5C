507473936d3e6ae354c5af301358606169030a96
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 13:42:30 +0700

    Add Variabel2.kt

---
+++ src/main/kotlin/Variabel2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " +dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " +staticVar)
+}
