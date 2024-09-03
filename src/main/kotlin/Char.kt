60feb093ac4cfa45382ef07cacbb16a9bf246013
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 14:54:47 +0700

    Add Char.kt

---
+++ src/main/kotlin/Char.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var charA : Char = 'A'
+//    println(charA == 65)
+    println(charA.isUpperCase())
+    println(charA.isLowerCase())
+    println(charA.isDigit())
+    println(charA.toLowerCase())
+    val strA: String = charA.toString()
+    println("Kini charA sudah menjadi String = "+strA)
+}
