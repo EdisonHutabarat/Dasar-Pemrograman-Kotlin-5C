48fd745881e598230175a8948acf92a7f7d12fda
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 12:26:03 +0700

    Add Literal.kt

---
+++ src/main/kotlin/Literal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+    println("intLiteral = " +intLiteral)
+    println("doubleLiteral = " +doubleLiteral)
+    println("stringLiteral = " +stringLiteral)
+    println("charLiteral = " +charLiteral)
+    println("boolLiteral = " +boolLiteral)
+}
