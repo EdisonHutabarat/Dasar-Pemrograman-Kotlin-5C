10b8949cf179a488b6a56fccb7c3a798b7b064a2
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 13:53:46 +0700

    Add Var vs Val Variabel

---
+++ src/main/kotlin/Val_Variabel.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varVariabel : Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning first value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+//    val phi = 3.14
+//    phi = 2.5
+}
---
+++ src/main/kotlin/Var_Variabel.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var varVariabel : Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning second valut to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = " +varVariabel)
+}
