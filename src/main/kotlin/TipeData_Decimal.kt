6a45478ba02c83521a5b147a9bb975c315cd6d9e
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 14:08:53 +0700

    Add TipeData_Decimal.kt

---
+++ src/main/kotlin/TipeData_Decimal.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+
+    println("maxFloat: "+maxFloat)
+    println("maxDouble: "+maxDouble)
+}
