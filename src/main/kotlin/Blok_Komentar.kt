8a83b7ba04d01ea40aeb2e9eef7840d8f39b5ef2
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-09-03 13:28:21 +0700

    Add Blok_Komentar.kt

---
+++ src/main/kotlin/Blok_Komentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Program Pertama Saya di Kotlin!")
+    //Contoh Komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
